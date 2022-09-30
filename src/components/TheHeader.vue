b6e3d17738ce36dd088f7f85e6ebf23f162948aa
Emmanuel Onyo <emmanuelonyo34@gmail.com>
2022-09-30 21:17:36 +0100

    first commit

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,15 @@
+<template>
+  <header class="text-center bg-blue-800 text-white p-4 mb-10">
+    <div class="text-3xl md:text-5xl font-bold mb3">
+      <i class="fa fa-viruses"></i> Covid 19 Tracker
+    </div>  
+    <p class="text-blue-300 ">Covid 19 Api by <a href="https://github.com/emmanuelonyo" target="_blank">Emmanuel Onyo</a></p>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'TheHeader',
+  props: { }
+}
+</script>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,18 +1,49 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main v-if="!loading">
+    <DateTitle :text="title" :dataDate="dataDate" />
+    <DataBoxes :stats="stats" />
+  </main>
+
+  <main class="flex flex-col align-center justify-center text-center" v-else>
+    <div class="text-grey-500 text-3xl mt-10 mb-6">
+      Fetching Data
+    </div>
+
+  </main>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+const axios = require('axios')
+import DateTitle from '@/components/DateTitle.vue'
+import DataBoxes from '@/components/DataBoxes.vue'
 
 export default {
   name: 'HomeView',
-  components: {
-    HelloWorld
+  components: { DateTitle, DataBoxes},
+  data () {
+    return {
+      loading:true,
+      title: 'Global',
+      dataDate: '',
+      status: {},
+      countries: [],
+      loadingImage: ''
+    }
+  },
+  methods: {
+    async fetchCovidData(){
+      const res = axios.get('https://api.covid19api.com/summary')
+      const data = await res
+      return data.data
+    }
+  },
+  async created() {
+    const data = await this.fetchCovidData()
+    this.dataDate = data.Date
+    this.stats = data.Global
+    this.countries = data.countries
+    this.loading = false
+
   }
 }
 </script>
